76960663a59ff26a7ecb278d8f9f66eb835027f3
Patryk Ptasinski <patryk@ipepe.pl>
2021-01-22 13:27:59 +0100

    add h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import styles from './App.module.css';
 import { Itunes } from './itunes';
+import { Stack } from '@chakra-ui/react';
 
 function App({ title }: { title: string }) {
   return (
     <div className={styles['App']}>
       <header className={styles['App-header']}>
-        <Itunes />
+        <Stack>
+          <h1>{title}</h1>
+          <Itunes />
+        </Stack>
       </header>
     </div>
   );
