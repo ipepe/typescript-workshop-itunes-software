b8889ae99c8db274d7ca51696ff637493f681898
Patryk Ptasinski <patryk@ipepe.pl>
2021-01-22 11:00:52 +0100

    Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Button, ChakraProvider} from '@chakra-ui/react';
+import { Button, ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
   <React.StrictMode>
